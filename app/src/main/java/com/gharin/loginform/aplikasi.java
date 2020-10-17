fc4533356b520d0ed9108d9dd96b4b35f109633d
Nu'man Nashif Annawwaf <meeannawwaf007@gmail.com>
2020-10-17 10:11:20 +0700

    Update aplikasi.java

--- app/src/main/java/com/gharin/loginform/aplikasi.java
+++ app/src/main/java/com/gharin/loginform/aplikasi.java
@@ -9,5 +9,7 @@ public class aplikasi extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aplikasi);
+        
+        //INI CONTOH UNTUK COMMENT
     }
 }
